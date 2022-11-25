2b8b9648b5ec833c5c50b1efec28c7e401bea7e4
Remi <112860220+Lolgitech@users.noreply.github.com>
2022-11-25 14:41:18 +0100

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,23 +1,28 @@
 const submitInput = () => {
-  const numbers = document.getElementById("inputValue").value;
-
-  if (numbers != 0 && numbers < 1000) {
-    const circleParent = document.getElementById("circles");
-    circleParent.innerHTML = "";
-    for (let i = 0; i < numbers; i++) {
-      setTimeout(drawCircle, 1000);
-      const child = document.createElement("div");
-      child.style.backgroundColor =
-        "#" + Math.floor(Math.random() * 16777215).toString(16);
-      child.style.borderColor =
-        "#" + Math.floor(Math.random() * 16777215).toString(16);
-      child.classList.add("circle");
-      child.innerHTML = i + 1;
-      circleParent.appendChild(child);
-    }
-  } else {
+	const numbers =  document.getElementById("inputValue").value
+  
+    if ((numbers!=0)&(numbers  < 1000)){
+  	const circleParent =  document.getElementById("circles")
+    circleParent.innerHTML = ""
+    for (let i = 0; i < (numbers); i++) {
+    setTimeout(drawCircle, 1000)
+  		const child = document.createElement("div");
+          child.style.backgroundColor =
+          "#" + Math.floor(Math.random() * 16777215).toString(16);
+          child.style.borderColor =
+          "#" + Math.floor(Math.random() * 16777215).toString(16);
+          child.classList.add("circle")
+      child.innerHTML = i+1
+      circleParent.appendChild(child)
+		}
+  } 
+  else {
     window.alert("Please input a number between 1 and 1000");
   }
-};
 
-const drawCircle = () => {};
+}
+
+const drawCircle = () => {
+
+}
+ 
